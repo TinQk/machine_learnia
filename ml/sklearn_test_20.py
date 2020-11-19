89f3bb7d6d8050044f103b0f70b10dd2477d7fb5
Quentin Potié <potie.quentin@gmail.com>
2020-11-19 16:39:27 +0100

    reclass

---
+++ ml/sklearn_test_20.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 19 16:09:38 2020
+
+@author: Quentin
+"""
+
+import numpy as np
+import pandas as pd
+
+import sklearn 
+
+# les x sont les "features" --> données en entrée, on peut en avoir plusieurs
+# les y sont les données en sortie
+
+# 1. sélectionner un model et préciser ses hyperparametres
+
+
+# 2. entrainer un model sur les données X, y via .fit
+
+
+# 3. évaluer le model via .score
+
+
+# 4. utiliser le model via .predict
